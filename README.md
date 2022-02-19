b49881d9508f2c3942f5a7d86ad793882e9d23d2
DaveCroteau <mr.dave.croteau@gmail.com>
2022-02-19 11:06:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Page d'accueil démo
+
+Ce site est une page d'accueil simulant une école de musique incluant un formulaire de contact
+
+## Démo live
+
+url à venir...
---
+++ script.js
@@ -0,0 +1,95 @@
+document.querySelector('#contact-btn').addEventListener('click', () => {
+	document.querySelector('.form-bg').classList.toggle('active')
+})
+
+document.querySelector('.form-bg').addEventListener('click', e => {
+	if (e.target.matches('.form-bg')) {
+		document.querySelector('.form-bg').classList.toggle('active')
+	}
+})
+
+function validateEmail(email) {
+	return email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) ? true : false
+}
+
+document.querySelector('#submit').addEventListener('click', e => {
+	e.preventDefault()
+
+	const form = document.querySelector('#contact-form')
+	const elements = [...form.elements].filter(el => el.type != 'button')
+	const data = elements.reduce((acc, el) => {
+		el.name === 'email' ? (acc[el.name] = el.value.toLowerCase().trim()) : (acc[el.name] = el.value.trim())
+		return acc
+	}, {})
+
+	// set red borders
+	elements.forEach(el => {
+		if (!el.value.trim()) {
+			document.querySelector(`#${el.id}`).classList.add('field-error')
+		}
+	})
+
+	const confirm = document.querySelector('.form-confirmation')
+
+	// return if missing fields
+	if (elements.filter(el => el.value === '').length > 0) {
+		confirm.classList.add('form-error', 'active')
+		confirm.textContent = 'Veuillez remplir tous les champs'
+		setTimeout(() => {
+			confirm.classList.remove('form-error', 'active')
+			setTimeout(() => {
+				confirm.textContent = ''
+			}, 200)
+		}, 5000)
+		return
+	}
+
+	// return if email not valid
+	if (!validateEmail(data.email)) {
+		document.querySelector('#email').classList.add('field-error')
+		confirm.classList.add('form-error', 'active')
+		confirm.textContent = 'Veuillez entrer un courriel valide'
+		setTimeout(() => {
+			confirm.classList.remove('form-error', 'active')
+			setTimeout(() => {
+				confirm.textContent = ''
+			}, 200)
+		}, 5000)
+		return
+	}
+
+	const formData = new FormData(form)
+	fetch('/', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+		body: new URLSearchParams(formData).toString(),
+	})
+		.then(() => {
+			form.reset()
+			confirm.classList.add('form-success', 'active')
+			confirm.textContent = 'Messgage envoyé'
+			setTimeout(() => {
+				confirm.classList.remove('form-success', 'active')
+				document.querySelector('.form-bg').classList.toggle('active')
+				setTimeout(() => {
+					confirm.textContent = ''
+				}, 200)
+			}, 5000)
+		})
+		.catch(error => console.log(error))
+})
+
+document.querySelector('#contact-form').addEventListener('input', e => {
+	if ([...e.target.classList].includes('field-error')) {
+		e.target.classList.remove('field-error')
+	}
+	if (e.target.value === '') {
+		e.target.classList.add('field-error')
+	}
+})
+
+document.querySelector('#email').addEventListener('blur', e => {
+	if (!validateEmail(e.target.value)) {
+		e.target.classList.add('field-error')
+	}
+})
